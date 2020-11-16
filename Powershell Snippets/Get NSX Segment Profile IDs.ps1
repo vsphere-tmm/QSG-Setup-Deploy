30cb7203af099260142eb7304f2024536bef7b13
mikefoley <mike@yelof.com>
2020-11-16 17:33:12 -0500

    Create Get NSX Segment Profile IDs.ps1

---
+++ Powershell Snippets/Get NSX Segment Profile IDs.ps1
@@ -0,0 +1,6 @@
+$cred = Get-Credential
+Connect-NsxtServer -server 10.172.212.15 -Credential $cred
+#Get-NsxtService -name com.vmware.nsx.switching_profiles
+#Get-NsxtService -name com.vmware.nsx.switching_profiles |get-member
+#(Get-NsxtService -name com.vmware.nsx.switching_profiles).list().results
+(Get-NsxtService -name com.vmware.nsx.switching_profiles).list().results |select id,display_name
